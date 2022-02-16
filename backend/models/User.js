54663bf0ef644b72d8aeef2ebc9c10972d290ce8
Evan Jannot <ejannot@ensc.fr>
2022-02-16 13:39:39 +0100

    unique password

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
 
+const uniqueValidator = require('mongoose-unique-validator')
+
 const userSchema = mongoose.Schema({
-  id_user: { type: String, required: true },
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   highscore: { type: Number, required: true, default: 0 },
 })
+
+userSchema.plugin(uniqueValidator)
 
 module.exports = mongoose.model('User', userSchema)
