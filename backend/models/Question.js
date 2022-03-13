cfb4a0f3d6b422eb8377a5328c5381ea89f31250
Evan Jannot <ejannot@ensc.fr>
2022-03-13 12:42:52 +0100

    Schema questions

--- backend/models/Question.js
+++ backend/models/Question.js
@@ -6,6 +6,9 @@ const questionSchema = mongoose.Schema(
   {
     question_statement: { type: String, required: true },
     question_answer: { type: String, required: true },
+    fake1: { type: String, required: true },
+    fake2: { type: String, required: true },
+    fake3: { type: String, required: true },
     points: { type: Number, required: true },
     theme: { type: String, required: true },
     credits: { type: String, required: true },
