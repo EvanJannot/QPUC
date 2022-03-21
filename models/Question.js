584f170c66d3d564356414fb9a569a4424722cde
Evan Jannot <79797037+EvanJannot@users.noreply.github.com>
2022-03-21 17:26:23 +0100

    Add files via upload

---
+++ models/Question.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const uniqueValidator = require('mongoose-unique-validator')
+
+const questionSchema = mongoose.Schema(
+  {
+    question_statement: { type: String, required: true },
+    question_answer: { type: String, required: true },
+    fake1: { type: String, required: true },
+    fake2: { type: String, required: true },
+    fake3: { type: String, required: true },
+    points: { type: Number, required: true },
+    theme: { type: String, required: true },
+    credits: { type: String, required: true },
+  },
+  {
+    versionKey: false,
+  }
+)
+
+questionSchema.plugin(uniqueValidator)
+
+module.exports = mongoose.model('Question', questionSchema)
---
+++ routes/question.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+const questionCtrl = require('../controllers/question')
+
+router.get('/', questionCtrl.getAllQuestions)
+router.get('/:id', questionCtrl.getOneQuestion)
+
+router.post('/addQ', questionCtrl.addQuestion)
+
+module.exports = router
