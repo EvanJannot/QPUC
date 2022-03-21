9e02ddc8aa214804bb04ce1e547ebde587cac7cf
Evan Jannot <ejannot@ensc.fr>
2022-03-21 13:11:48 +0100

    Calcul score

--- frontend/src/pages/Score/index.jsx
+++ frontend/src/pages/Score/index.jsx
@@ -33,7 +33,7 @@ function Score() {
 
   useEffect(() => {
     window.scrollTo(0, 0)
-    let calculus = score * (450 - time) - errors * time
+    let calculus = score * (1000 - time) - errors * 100
     setFinalScore(calculus)
     fetch(`http://localhost:4200/api/auth`)
       .then((response) => response.json())
